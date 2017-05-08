2486061331db0db3d2e4e383d78daca3cde36d37
hjdo2 <hjdo2@illinois.edu>
2017-05-08 01:30:45 -0500

    buttons for surveys

---
+++ static/js/postsurvey.js
@@ -0,0 +1,28 @@
+function popup(interface,post){
+  var link="";
+if (interface=="w" && post=="1"){
+  link="https://goo.gl/forms/IGkiVFpYqP98tPdo2";
+ }
+ else if (interface=="w" && post=="2"){
+  link="https://goo.gl/forms/IGkiVFpYqP98tPdo2";
+ }
+else if (interface=="w" && post=="3"){
+  link="https://goo.gl/forms/IGkiVFpYqP98tPdo2";
+ }
+else  if (interface=="w" && post=="5"){
+  link="https://goo.gl/forms/IGkiVFpYqP98tPdo2";
+ }
+else  if (interface=="c" && post=="1"){
+  link="https://goo.gl/forms/IGkiVFpYqP98tPdo2";
+ }
+else  if (interface=="c" && post=="2"){
+  link="https://goo.gl/forms/IGkiVFpYqP98tPdo2";
+ }
+else  if (interface=="c" && post=="3"){
+  link="https://goo.gl/forms/IGkiVFpYqP98tPdo2";
+ }
+else  if (interface=="c" && post=="5"){
+  link="https://goo.gl/forms/IGkiVFpYqP98tPdo2";
+ }
+  cuteLittleWindow = window.open(link, "littleWindow", "location=no,width=760,height=500");
+}
