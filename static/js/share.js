73b6d638d0917e3e1236b82c16cb429c75b033f4
Hyo Jin Do <hjdo2@illinois.edu>
2018-08-17 13:07:29 -0500

    share button

--- static/js/share.js
+++ static/js/share.js
@@ -1,9 +1,11 @@
 function shareFunction(shareid) {
+     var whowhy;
+     whowhy_data= prompt("Describe the person you want to share this comment with and explain why","");
      
      var id_share=shareid.split("\t\t");
 
      alert("Shared!")
-	$.post( "/share", { 'shareid[]': id_share} );
+	 $.post( "/share", { 'shareid[]': id_share, whowhy: whowhy_data} );
      
 	 
     }
